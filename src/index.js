341e21a22b827cad706f77da000b87cba4241870
Josh Black <joshblack@us.ibm.com>
2016-03-20 19:27:07 -0500

    Adding Schema example

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import {
-  GraphQLSchema,
-  GraphQLString,
-  GraphQLObjectType
-} from 'graphql';
+import schema from './schema';
 
 const PORT = 3000;
 const app = express();
 
-const MyFirstSchema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        name: 'HelloField',
-        type: GraphQLString,
-        description: 'Hello world from documentation',
-        resolve: () => 'world',
-      },
-    },
-  }),
-});
-
 app.use('/graphql', graphqlHTTP({
-  schema: MyFirstSchema,
+  schema,
   graphiql: true,
 }));
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log(`Server listening on http://localhost:${PORT}/graphql`);
 });
 
